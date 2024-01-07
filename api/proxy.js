6c6bc7e6ceecb49705312f77d3aa2d89d22a3ac8
LS-Ze <papningyuan@outlook.com>
2024-01-07 12:40:49 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -13,5 +13,11 @@ module.exports = (req, res) => {
     // 创建代理对象并转发请求
     createProxyMiddleware({
         target,
+        changeOrigin: true,
+        pathRewrite: {
+            // 通过路径重写，去除请求路径中的 `/api`
+            // 如果开启了,那么 /api/user/login 将被转发到 http://gmall-h5-api.atguigu.cn/user/login
+            //'^/json/stats.json': '/',
+        },
     })(req, res)
 }
